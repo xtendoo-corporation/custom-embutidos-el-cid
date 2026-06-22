61eb7fd7a1088f05540df8647ad4b89ddde8b70a
Gokac9 <theabraham9@gmail.com>
2026-06-22 10:00:44 +0200

    archive studio views stock , and optimize stock move report

--- embutidoscid_document_format/hooks.py
+++ embutidoscid_document_format/hooks.py
@@ -1,5 +1,8 @@
 def deactivate_web_studio_views(env):
-    view_ids = [2596, 2651, 2657]
+    view_ids = [
+        2596, 2651, 2657,  # sale order
+        2598, 2600, 2602, 2604, 2606, 2624, 2628, 2630,  # stock picking
+    ]
     views = env['ir.ui.view'].browse(view_ids).exists()
     if views:
         views.write({'active': False})
--- embutidoscid_document_format/migrations/19.0.1.0.0/post-migrate.py
+++ embutidoscid_document_format/migrations/19.0.1.0.0/post-migrate.py
@@ -1,2 +1,2 @@
 def migrate(cr, version):
-    cr.execute("UPDATE ir_ui_view SET active=False WHERE id IN (2596,2651,2657)")
+    cr.execute("UPDATE ir_ui_view SET active=False WHERE id IN (2596,2651,2657,2598,2600,2602,2604,2606,2624,2628,2630)")
