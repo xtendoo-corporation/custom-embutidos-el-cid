4ebca87978c92bdec26229ffd8d7d6b39bf0fdf3
Gokac9 <theabraham9@gmail.com>
2026-07-14 10:58:21 +0200

    en impresion de albaran quitar fecha de caducidad y desglosar el recargo

--- embutidoscid_document_format/models/stock_move.py
+++ embutidoscid_document_format/models/stock_move.py
@@ -23,6 +23,16 @@ class StockMove(models.Model):
         compute="_compute_sale_prices",
         currency_field="sale_currency_id",
     )
+    sale_price_tax_iva = fields.Monetary(
+        string="IVA",
+        compute="_compute_sale_prices",
+        currency_field="sale_currency_id",
+    )
+    sale_price_tax_re = fields.Monetary(
+        string="Recargo",
+        compute="_compute_sale_prices",
+        currency_field="sale_currency_id",
+    )
     sale_currency_id = fields.Many2one(
         "res.currency",
         compute="_compute_sale_prices",
@@ -51,11 +61,31 @@ class StockMove(models.Model):
                 move.sale_price_total = sale_line.price_total * qty_ratio
                 move.sale_currency_id = sale_line.currency_id
                 move.sale_tax_id = sale_line.tax_ids
+
+                # Desglose de impuestos (IVA y Recargo)
+                iva_amount = 0.0
+                re_amount = 0.0
+                taxes_res = sale_line.tax_ids.compute_all(
+                    sale_line.price_unit * (1 - (sale_line.discount or 0.0) / 100.0),
+                    quantity=move.quantity,
+                    product=move.product_id,
+                    partner=sale_line.order_id.partner_id
+                )
+                for tax_val in taxes_res['taxes']:
+                    tax = self.env['account.tax'].browse(tax_val['id'])
+                    if tax.tax_group_id and 'recargo' in tax.tax_group_id.name.lower():
+                        re_amount += tax_val['amount']
+                    else:
+                        iva_amount += tax_val['amount']
+
+                move.sale_price_tax_iva = iva_amount
+                move.sale_price_tax_re = re_amount
             else:
                 move.sale_price_unit = 0.0
                 move.sale_discount = 0.0
                 move.sale_price_subtotal = 0.0
                 move.sale_price_total = 0.0
+                move.sale_price_tax_iva = 0.0
+                move.sale_price_tax_re = 0.0
                 move.sale_currency_id = False
                 move.sale_tax_id = False
-
